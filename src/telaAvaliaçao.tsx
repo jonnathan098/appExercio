0806debe26d1fc0a124dfad5345a697fcd538515
jonnathan <jhonthancarlos@gmail.com>
2022-08-03 21:54:01 -0300

    oi

---
+++ src/telaAvaliaçao.tsx
@@ -0,0 +1,11 @@
+import { View,Text } from "react-native"
+
+const TelaAvaliaçao: React.FC= () => {
+    return(
+        <View>
+            <Text>ola</Text>
+        </View>
+    )
+}
+
+export default TelaAvaliaçao
