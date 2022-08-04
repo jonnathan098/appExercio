6042ec85a55c800bdc1f263dd77cf06f2052db91
jonnathan <jhonthancarlos@gmail.com>
2022-08-03 21:53:30 -0300

    criando novo  app

---
+++ src/telaInicaial.tsx
@@ -0,0 +1,12 @@
+import { View, Text, Button} from "react-native"
+
+const TelaInicial: React.FC = () => {
+    return(
+        <View>
+            <Text>ola bem vinto </Text>
+            <Button title="ola bora começar" onPress={() => {}} />
+        </View>
+    )
+}
+
+export default TelaInicial
